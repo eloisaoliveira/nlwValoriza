973cf793ba7aadd9262e6279cce5dc334b0da499
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-06-23 20:47:38 -0300

    created the route file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import "reflect-metadata";
 import express from "express";
 
+import { router } from "./routes"
+
 import "./database";
 
 const app = express();
+
+app.use(express.json())
+
+app.use(router)
 
 app.listen(3000, () => console.log("Server is running"));
