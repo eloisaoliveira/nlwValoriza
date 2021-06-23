d01d66e6e90a7b879db7be1a1c77566b8056fe58
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-06-22 23:46:44 -0300

    configured the database

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
-// @types/express
+import "./database";
+
 const app = express();
 
-app.get("/test", (req, res) => {
-    return res.send("Olá NLW")
-})
-
-app.post("/test-post", (req, res) => {
-    return res.send("Olá NLW método POST")
-})
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
