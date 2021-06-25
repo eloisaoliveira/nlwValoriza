e0adbc0803daee6f3309982003ce59d9a6ef772f
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-06-25 19:38:43 -0300

    feat(ensureAdmin): only admin that can register a tag

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express"
 import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateTagController } from "./controllers/CreateTagController"
+import { ensureAdmin } from "./middlewares/ensureAdmin"
 
 const router = Router()
 
 const createUserController = new CreateUserController
 const createTagController = new CreateTagController
 
-router.post("/users", createUserController.handle)
-router.post("/tags", createTagController.handle)
+router.post("/users",  createUserController.handle)
+router.post("/tags", ensureAdmin, createTagController.handle)
 
 export { router }
