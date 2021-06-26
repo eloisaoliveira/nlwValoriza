cb35f686e8e5bdb72545123247fe99ff27a6a073
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-06-26 11:42:04 -0300

    feat(User): encrypting user password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,6 @@
 import { getCustomRepository } from "typeorm"
 import { UsersRepositories } from "../repositories/UsersRepositories"
-
+import { hash } from "bcryptjs"
 interface IUserRequest {
     name: string 
     email: string 
@@ -24,11 +24,13 @@ class CreateUserService {
             throw new Error("User already exists")
         }
 
+        const passwordHash = await hash(password, 8)
+
         const user = usersRepository.create({
             name,
             email,
             admin,
-            password
+            password: passwordHash
         })
 
         await usersRepository.save(user)
