25f1b00e7f18e25bda4f8fe26507609246b7efd3
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-06-26 20:41:46 -0300

    structure created for listing of compliments sent and received

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import { CreateTagController } from "./controllers/CreateTagController"
 import { ensureAdmin } from "./middlewares/ensureAdmin"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 import { CreateComplimentController } from "./controllers/CreateComplimentController"
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController"
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController"
 
 const router = Router()
 
@@ -11,10 +14,15 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listUserSendComplimentsController = new ListUserSendComplimentsController()
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
 
 router.post("/users",  createUserController.handle)
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
-router.post("/compliments", createComplimentController.handle)
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
+router.get("/users/compliments/receiver",ensureAuthenticated, listUserReceiveComplimentsController.handle)
 
 export { router }
