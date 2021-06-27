c7dabfb67cf599452c12b7797aee8b75c800dba9
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-06-26 21:53:08 -0300

    feat(User): function delete password exposure

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,5 @@
 import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import { Exclude } from "class-transformer"
 import { v4 as uuid } from "uuid"
 
 @Entity("users")
@@ -16,6 +17,7 @@ class User {
     @Column()
     admin: boolean
 
+    @Exclude()
     @Column()
     password: string
 
